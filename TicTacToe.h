6f14c6933fefa568a73d1d13397eef793b6ac093
ecyht2 <ecyht2@nottingham.edu.my>
2021-12-25 14:16:11 +0800

    Changed programs to use header files

---
+++ TicTacToe.h
@@ -0,0 +1,19 @@
+#ifndef TICTACTOE_H_
+#define TICTACTOE_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int check_win(int board[]);
+char player_symbol(int playerNo);
+int ask_no_players();
+int ask_bot_diff(int botNo);
+void copy_board(int inBoard[], int outBoard[]);
+int bot_choice(int board[], int botDiff, int player);
+int ask_placement(int player, int botDiff, int noPlayers, int board[]);
+void print_board(int board[]);
+int playGame(int noPlayers, int botDiff[]);
+void printWins(int wins[], char *message);
+void exportWins(int wins[], char *message);
+
+#endif // TIC-TAC-TOE_H_
--- make
+++ make
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 vg="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --track-origins=yes -s"
 FILE="Tan_HongKai_assessment2_EEEE1042"
+INCLUDE="TicTacToe.c"
 
-gcc -o $FILE.o $FILE.c
-gcc -o $FILE.1.o $FILE.1.c
-gcc -o $FILE.2.o $FILE.2.c
-gcc -o $FILE.3.o $FILE.3.c
+gcc $FILE.c $INCLUDE -o $FILE.o
+gcc $FILE.1.c $INCLUDE -o $FILE.1.o
+gcc $FILE.2.c $INCLUDE -o $FILE.2.o
+gcc $FILE.3.c $INCLUDE -o $FILE.3.o
 $vg ./$FILE.2.o
