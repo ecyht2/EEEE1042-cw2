cacaf68cf8ea4ca7318c1f58fe3dba096a66f265
ecyht2 <ecyht2@nottingham.edu.my>
2022-01-03 10:08:32 +0800

    Organized the code better

--- Tan_HongKai_assessment2_EEEE1042.3.c
+++ Tan_HongKai_assessment2_EEEE1042.3.c
@@ -14,40 +14,29 @@ int main(){
   // Setting Bot/AI Difficulty
   /* botDiff[0] sets value for player "x", botDiff[1] sets values for player "o"
     Value of 0 means the AI is playered control, 1 means a Random AI and 2 means Smart AI */
-  int botDiff[2] = {2, 2};
+  int botDiff[4][2] = {
+    {3, 3},
+    {2, 2},
+    {2, 1},
+    {1, 1}
+  };
+  char modes[4][20] = {
+    "Minimax vs Minimax",
+    "Smart vs Smart",
+    "Smart vs Random",
+    "Random vs Random"
+  };
 
-  int wins[3] = {0};
-  int cWinner;
-  for(int i = 0; i < N; i++){
-    cWinner = playGame(noPlayers, botDiff);
-    wins[cWinner]++;
+  for(int i = 0; i < 4; i++){
+    int wins[3] = {0};
+    int cWinner;
+    for(int j = 0; j < N; j++){
+      cWinner = playGame(noPlayers, botDiff[i]);
+      wins[cWinner]++;
+    }
+    printWins(wins, modes[i]);
+    exportWins(wins, modes[i]);
   }
-  printWins(wins, "Smart vs Smart");
-  exportWins(wins, "Smart vs Smart");
-
-  /* Smart AI vs Random AI */
-  // Setting Bot/AI Difficulty
-  botDiff[1] = 1;
-
-  for(int i = 0; i < 3; i++) wins[i] = 0;
-  for(int i = 0; i < N; i++){
-    cWinner = playGame(noPlayers, botDiff);
-    wins[cWinner]++;
-  }
-  printWins(wins, "Smart vs Random");
-  exportWins(wins, "Smart vs Random");
-
-  /* Random AI vs Random AI */
-  // Setting Bot/AI Difficulty
-  botDiff[0] = 1;
-
-  for(int i = 0; i < 3; i++) wins[i] = 0;
-  for(int i = 0; i < N; i++){
-    cWinner = playGame(noPlayers, botDiff);
-    wins[cWinner]++;
-  }
-  printWins(wins, "Random vs Random");
-  exportWins(wins, "Random vs Random");
 
   return 0;
 }
