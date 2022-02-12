54f882dd4fb383f1dc0e411f8cfbdd676d3f1378
ecyht2 <ecyht2@nottingham.edu.my>
2022-02-12 16:28:53 +0800

    Updated all the files to the newest version

--- make
+++ make
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 vg="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --track-origins=yes -s"
-FILE="Tan_HongKai_assessment2_EEEE1042"
+FILE="ttt"
 
 gcc -o $FILE.o $FILE.c
 gcc -o $FILE.1.o $FILE.1.c
--- ttt.c
+++ ttt.c
@@ -1,21 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-const int winCon[][3] = {
-  //Horizontal Win Conditions
-  {0, 1, 2},
-  {3, 4, 5},
-  {6, 7, 8},
-  //Vertical Win Conditions
-  {0, 3, 6},
-  {1, 4, 7},
-  {2, 5, 8},
-  //Diaganol Win Conditions
-  {0, 4, 8},
-  {2, 4, 6}
-};
+
+int check_win(int board[]);
+void switch_player(int *player);
+char player_symbol(int playerNo);
+int ask_no_players(void);
+int ask_bot_diff(int botNo);
+int minimax(int board[], int player, int nFreeSpace, int freeSpace[], int * choice);
+int bot_choice(int board[], int botDiff, int player);
+int bot_logic(int board[], int botDiff[], int noPlayers, int player);
+int ask_placement(int player, int botDiff[], int noPlayers, int board[]);
+void print_board(int board[]);
+void print_winner(int winner);
+int playGame(int noPlayers, int botDiff[]);
+void printWins(int wins[], char *message);
+void exportWins(int wins[], char *message);
+
+int main(){
+  /* The program will first ask you the amount of players that are playing, 2 = two player mode, 1 = single player mode and 0 = AI vs AI */
+  /* If the amount of player chosen is less than 2, the program will ask you what the difficulty of AI you want to be. 1 = the AI will place randomly and 2 = will be the smart AI which has some logic */
+
+  // Setting up random seed
+  srand(clock());
+  // Asking for how many players are playing
+  int noPlayers = ask_no_players();
+
+  // Asking for the difficulty of the bot
+  /* botDiff[0] sets value for player "x", botDiff[1] sets values for player "o"
+    Value of 0 means the AI is playered control, 1 means a Random AI and 2 means Smart AI */
+  int botDiff[2] = {0};
+
+  if(noPlayers == 1){
+    botDiff[1] = ask_bot_diff(2);
+  } else if(noPlayers == 0){
+    botDiff[0] = ask_bot_diff(1);
+    botDiff[1] = ask_bot_diff(2);
+  }
+
+  playGame(noPlayers, botDiff);
+
+  return 0;
+}
 
 int check_win(int board[]){
+  const int winCon[][3] = {
+    //Horizontal Win Conditions
+    {0, 1, 2},
+    {3, 4, 5},
+    {6, 7, 8},
+    //Vertical Win Conditions
+    {0, 3, 6},
+    {1, 4, 7},
+    {2, 5, 8},
+    //Diaganol Win Conditions
+    {0, 4, 8},
+    {2, 4, 6}
+  };
+
   // Setting up the number of win Conditions
   int noWinCon = sizeof(winCon)/sizeof(int)/3;
 
@@ -29,6 +71,10 @@ int check_win(int board[]){
 
   // Returing that no one has won yet
   return 0;
+}
+
+void switch_player(int *player){
+  *player = *player % 2 + 1;
 }
 
 char player_symbol(int playerNo){
@@ -72,22 +118,75 @@ int ask_bot_diff(int botNo){
   player = player_symbol(botNo);
 
   do{
-    printf("Available Choices: 1, 2\n");
+    printf("Available Choices: 1, 2, 3\n");
     printf("What difficulty do you want bot %c to be: ", player);
     if(scanf("%d", &bDiff) == 0) scanf("%*s");
     printf("\n");
 
-    if(bDiff < 1 || bDiff > 2){
+    if(bDiff < 1 || bDiff > 3){
       printf("Invalid Bot/AI difficulty, please try again\n");
     }
-  } while(bDiff < 1 || bDiff > 2);
+  } while(bDiff < 1 || bDiff > 3);
 
   // Returning the Bot difficulty
   return bDiff;
 }
 
-void copy_board(int inBoard[], int outBoard[]){
-  for(int i = 0; i < 9; i++) outBoard[i] = inBoard[i];
+int minimax(int board[], int player, int nFreeSpace, int freeSpace[], int *choice){
+  // Checking current game state
+  int winner = check_win(board);
+  // Returns a value if there aren't anymore free spaces
+  if(nFreeSpace == 0 || winner > 0){
+    // Returns 0 if a tie
+    if(winner == 0)
+      return 0;
+    // Returns 10 if player won
+    else if(winner == player)
+      return 10;
+    // Returns -10 if player lost
+    else return -10;
+  }
+
+  // Setting up who is the next player
+  int nextPlayer = player;
+  if(player == 1)
+    nextPlayer = 2;
+  else nextPlayer = 1;
+
+  // Setting up variables
+  int newFreeSpace[nFreeSpace - 1];
+  int dummyChoice;
+  int cMax = -1000;
+
+  // Looping over all available choices
+  for(int i = 0; i < nFreeSpace; i++){
+    // Making a temporary choice
+    int tmpChoice = freeSpace[i];
+    board[tmpChoice] = player;
+
+    // Creating the left over free spaces
+    for(int j = 0; j < nFreeSpace; j++){
+      int remove = 0;
+      if(j >= i){
+        remove = 1;
+        if(j == i) continue;
+      }
+      newFreeSpace[j - remove] = freeSpace[j];
+    }
+
+    // Going over all possible choices
+    int value = -minimax(board, nextPlayer, nFreeSpace - 1, newFreeSpace, &dummyChoice);
+    // If the current choice have a higher score, use the current choice
+    if(value > cMax){
+      *choice = tmpChoice;
+      cMax = value;
+    }
+
+    // Undoing the temporary choice
+    board[tmpChoice] = 0;
+  }
+
+  return cMax;
 }
 
 int bot_choice(int board[], int botDiff, int player){
@@ -95,7 +194,6 @@ int bot_choice(int board[], int botDiff, int player){
   int *freeSpots = NULL;
   int counter = 0;
   int choice;
-  int tmpBoard[9] = {0};
 
   for(int i = 0; i < 9; i++){
     if(board[i] == 0){
@@ -124,13 +222,15 @@ int bot_choice(int board[], int botDiff, int player){
 
       // Checking if any of the choices will result in the bot's victory
       for(int i = 0; i < counter; i++){
-        //Creating a temporary new board
-        copy_board(board, tmpBoard);
-        //Setting the temporary board with the test value
+        // Testing Placments
         choice = freeSpots[i];
-        tmpBoard[choice] = player;
+        board[choice] = player;
+        // Checking winner
+        int winner = check_win(board);
+        // Undo Placment
+        board[choice] = 0;
         //Returning the choice if it results in a win
-        if(check_win(tmpBoard) == player){
+        if(winner == player){
           //Freeing memory from freeSpots
           free(freeSpots);
           return choice;
@@ -143,13 +243,15 @@ int bot_choice(int board[], int botDiff, int player){
 
       // Checking if any of the choices will result in the enemy's victory
       for(int i = 0; i < counter; i++){
-        //Creating a temporary new board
-        copy_board(board, tmpBoard);
-        //Setting the temporary board with the test value
+        // Testing Placments
         choice = freeSpots[i];
-        tmpBoard[choice] = player;
+        board[choice] = player;
+        // Checking winner
+        int winner = check_win(board);
+        // Undo Placment
+        board[choice] = 0;
         //Returning the choice if it results in an enemy win
-        if(check_win(tmpBoard) == player){
+        if(winner == player){
           //Freeing memory from freeSpots
           free(freeSpots);
           return choice;
@@ -164,31 +266,47 @@ int bot_choice(int board[], int botDiff, int player){
       return choice;
       break;
     case 3:
-      return 0;
+      minimax(board, player, counter, freeSpots, &choice);
+      free(freeSpots);
+      return choice;
+      break;
+    default:
+      free(freeSpots);
+      break;
   }
 
   return 0;
 }
 
-int ask_placement(int player, int botDiff, int noPlayers, int board[]){
-  // Setting up initial Values
+int bot_logic(int board[], int botDiff[], int noPlayers, int player){
   char playerSym = player_symbol(player);
   int choice = -1;
 
   // If there is no human players the choice would be chosen by an AI based on it's difficulty
   if(noPlayers == 0){
     // Finding and ruturning the bot choice
-    choice = bot_choice(board, botDiff, player);
+    choice = bot_choice(board, botDiff[player - 1], player);
     // Printing the choice of the bot
     printf("Bot %c chose spot number %d\n", playerSym, choice + 1);
     return choice;
   } else if(noPlayers == 1 && playerSym == 'o'){ //If the number players is one and the symbol is 'o' let an AI decide the choice
     // Finding and ruturning the bot choice
-    choice = bot_choice(board, botDiff, player);
+    choice = bot_choice(board, botDiff[player - 1], player);
     // Printing the choice of the bot
     printf("Bot %c chose spot number %d\n", playerSym, choice + 1);
     return choice;
   }
+
+  return -1;
+}
+
+int ask_placement(int player, int botDiff[], int noPlayers, int board[]){
+  // Setting up initial Values
+  char playerSym = player_symbol(player);
+  int choice = -1;
+
+  choice = bot_logic(board, botDiff, noPlayers, player);
+  if(choice != -1) return choice;
 
   do{
     // Asking where the player wants to place their symbol
@@ -239,38 +357,18 @@ void print_board(int board[]){
   printf("\n");
 }
 
-int playGame(int noPlayers, int botDiff[]);
-
-int main(){
-  /* The program will first ask you the amount of players that are playing, 2 = two player mode, 1 = single player mode and 0 = AI vs AI */
-  /* If the amount of player chosen is less than 2, the program will ask you what the difficulty of AI you want to be. 1 = the AI will place randomly and 2 = will be the smart AI which has some logic */
-
-  // Setting up random seed
-  srand(clock());
-  // Asking for how many players are playing
-  int noPlayers = ask_no_players();
-
-  // Asking for the difficulty of the bot
-  /* botDiff[0] sets value for player "x", botDiff[1] sets values for player "o"
-    Value of 0 means the AI is playered control, 1 means a Random AI and 2 means Smart AI */
-  int botDiff[2] = {0};
-
-  if(noPlayers == 1){
-    botDiff[1] = ask_bot_diff(2);
-  } else if(noPlayers == 0){
-    botDiff[0] = ask_bot_diff(1);
-    botDiff[1] = ask_bot_diff(2);
-  }
-
-  playGame(noPlayers, botDiff);
-
-  return 0;
+void print_winner(int winner){
+  char winnerChar = player_symbol(winner);
+  if(winner > 0) printf("Player %c won!\n", winnerChar);
+  else printf("It is a tie\n");
 }
 
 int playGame(int noPlayers, int botDiff[]){
   // Setting up initial values
   int cPlayer = rand() % 2 + 1;
-  int cBoard[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int cBoard[] = {0, 0, 0,
+                  0, 0, 0,
+                  0, 0, 0};
   int winner = 0;
 
   //Printing out the initial board
@@ -278,7 +376,7 @@ int playGame(int noPlayers, int botDiff[]){
   //Starting the game
   for(int i = 0; i < 9; i++){
     // Asking for the player where they want to place their symbol
-    int placement = ask_placement(cPlayer, botDiff[cPlayer - 1], noPlayers, cBoard);
+    int placement = ask_placement(cPlayer, botDiff, noPlayers, cBoard);
     // Updating the Board
     cBoard[placement] = cPlayer;
     //Printing the Board
@@ -289,14 +387,29 @@ int playGame(int noPlayers, int botDiff[]){
       break;
     }
     // Switching to next player
-    if(cPlayer == 1) cPlayer = 2;
-    else cPlayer = 1;
+    switch_player(&cPlayer);
   }
 
-  // Determining Winner
-  char winnerChar = player_symbol(winner);
-  if(winner > 0) printf("Player %c won!\n", winnerChar);
-  else printf("It is a tie\n");
+  // Printing Winner
+  print_winner(winner);
 
   return winner;
+}
+
+void printWins(int wins[], char *message){
+  printf("%s\n", message);
+  printf("x wins %d amount of times\n", wins[1]);
+  printf("x losts %d amount of times\n", wins[2]);
+  printf("It is a tie %d amount of times\n", wins[0]);
+}
+
+void exportWins(int wins[], char *message){
+  FILE *f;
+
+  if((f = fopen("outputData.txt", "a")) != NULL){
+    fprintf(f, "%s\n", message);
+    fprintf(f, "x wins %d amount of times\n", wins[1]);
+    fprintf(f, "x losts %d amount of times\n", wins[2]);
+    fprintf(f, "It is a tie %d amount of times\n", wins[0]);
+  } else printf("Failed to open output file");
 }
